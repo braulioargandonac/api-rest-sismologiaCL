f46a8f82f794443741d1cce52ef7fc2fd2f32326
kvn <kevin.salinas.2h@gmail.com>
2021-07-10 23:55:23 -0400

    se ingresan las nuevas tablas

--- database/database.sql
+++ database/database.sql
@@ -1,11 +1,20 @@
 create database sismologia;
 
 create table users(
-    id serial primary key not null, 
-    name varchar(255) not null,
-    email text not null
+    id serial primary key not null,
+    nombre text not null,            --text permite contener cadenas de longitud ilimitada
+    email text not null,
+    pass text not null
 );
 
-insert into users(name, email) values
-    ('kevin', 'kevin.salinasv@utem.cl'),
-    ('kvn', 'ljrflkfdj@gmail.com');
+create table sismo(
+    id serial primary key not null,
+    fecha_local text not null, 
+    fecha_utc text not null,
+    latitud text not null,
+    longitud text not null,
+    profundidad text not null,
+    magnitud text not null,
+    agencia text not null,
+    referencia text not null
+);
