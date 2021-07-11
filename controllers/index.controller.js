ee5a3be6c484a6e09f1cb8805e3a67729fa885b5
kvn <kevin.salinas.2h@gmail.com>
2021-07-10 22:05:21 -0400

    se arregla password de postgresql

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
     host: 'localhost',
     user: 'postgres',
-    password: '123',
+    password: 'postgres',
     database: 'sismologia',
     port: '5432'
 });
