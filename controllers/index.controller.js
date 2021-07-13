9db80451ddf85ae6ba13a9da270a3db8fc77f9cf
Brau <braulioargandonac@utem.cl>
2021-07-12 22:00:12 -0400

    Implementacion de try catch

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -24,9 +24,14 @@ const main = async (req, res) => {
         array.push(sismo);
     });
     await updateSismos(array);
-
-    const response = await pool.query('select * from sismo');
-    res.status(200).json(response.rows);
+    try{
+        const response = await pool.query('select * from sismo order by id;');
+        await res.status(200).json(response.rows);
+    }catch{
+        res.status(401).json({
+            message: 'Sin autorización',
+        });
+    }
 }
  
 
@@ -39,26 +44,30 @@ const pool = new Pool({
 });
 
 const createUsers = async (req, res) => {
-    const { nombre, email, pass } = req.body;
-    const response = await pool.query('insert into users (nombre, email, pass) values ($1, $2, $3)', [nombre, email, pass]);
-    console.log(response);
-    res.json({
-        message: 'Usuario creado con exito',
-        body:{
-            user: {nombre, email, pass}
-        }
-    })
+    try{
+        const { nombre, email, pass } = req.body;
+        const response = await pool.query('insert into users (nombre, email, pass) values ($1, $2, $3)', [nombre, email, pass]);
+        console.log(response);
+        res.json({
+            message: 'Usuario creado con exito',
+            body:{
+                user: {nombre, email, pass}
+            }
+        })
+    }catch{
+        res.status(412).json({
+            message: 'Precondición Fallida',
+        });
+    }
 };
 
 async function updateSismos(array){
-
     const response = await pool.query('select count(*) from sismo');
     if(response.rows[0].count > 14){
         for(let i=0; i<array.length; i++){
             const response1 = await pool.query('update sismo set fecha_local=$1, latitud=$2, longitud=$3, profundidad=$4, magnitud=$5, referencia=$6 where id=$7;', [array[i].fecha_local, array[i].latitud, array[i].longitud, array[i].profundidad, array[i].magnitud, array[i].referencia,i+1])
         }
     }else{
-        console.log("INSERTANDO DATOS...")
         for(let i=0; i<array.length; i++){
             const response2 = await pool.query('insert into sismo  (fecha_local, latitud, longitud, profundidad, magnitud, referencia) values ($1, $2, $3, $4, $5, $6)', [array[i].fecha_local, array[i].latitud, array[i].longitud, array[i].profundidad, array[i].magnitud, array[i].referencia ])
         }
@@ -66,21 +75,38 @@ async function updateSismos(array){
 };
 
 const getUsers = async (req, res) => {
-    
-    const response = await pool.query('select * from users');
-    res.status(200).json(response.rows);
+    try{
+        const response = await pool.query('select * from users');
+        res.status(200).json(response.rows);
+    }catch{
+        res.status(401).json({
+            message: 'Sin autorización',
+        });
+    }
 }
 
 const getSismosById = async (req, res) => {
-    const id = req.params.id;
-    const response = await pool.query('select * from sismo where id = $1', [id]);
-    res.status(200).json(response.rows);
+    try{
+        const id = req.params.id;
+        const response = await pool.query('select * from sismo where id = $1', [id]);
+        res.status(200).json(response.rows);
+    }catch{
+        res.status(404).json({
+            message: 'No se encuentra',
+        });
+    }
 }
 
 const getUsersById = async (req, res) => {
-    const id = req.params.id;
-    const response = await pool.query('select * from users where id = $1', [id]);
-    res.status(200).json(response.rows);
+    try{
+        const id = req.params.id;
+        const response = await pool.query('select * from users where id = $1', [id]);
+        res.status(200).json(response.rows);
+    }catch{
+        res.status(404).json({
+            message: 'No se encuentra',
+        });
+    }
 }
 
 module.exports = {
